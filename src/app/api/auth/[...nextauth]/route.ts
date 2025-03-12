27c5df22ba84369211b860835879e7e833b57bec
Fatey96 <rajwardhan714@gmail.com>
2025-03-12 17:50:14 -0400

    Fix duplicate NextAuth route by removing TypeScript version

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,6 +0,0 @@
-import NextAuth from 'next-auth';
-import { authOptions } from '../../../../../utils/auth';
-
-// Export the NextAuth handler with the GET and POST methods
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST }; 
